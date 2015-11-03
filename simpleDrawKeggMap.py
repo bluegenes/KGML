ab0321be7fbdfe89007e61bf2510b7a7374b0d37
Tessa Pierce <ntpierce@ucsd.edu>
2015-11-03 10:49:30 -0800

    simpleDrawKeggMap.py

--- simpleDrawKeggMap.py
+++ simpleDrawKeggMap.py
@@ -19,19 +19,6 @@ psr.add_argument('--blastKO', help='Kegg Orthology from blast', dest='KO')
 args = psr.parse_args()
 ##############################
 
-if not os.path.exists(args.outDir):
-    os.makedirs(args.outDir)
-
-pathway = retrieve_KEGG_pathway(args.path) #pathway of interest
-#pathway = read(args.path)
-#import pdb;pdb.set_trace()
-
-#get KO numbers from previously created file
-ko = ['ko:'+ ko for (trans,sp,ko) in (line.strip().split("\t") for line in (open(args.KO, 'r')))]
-
-#knownKOSet = set([e for e in pathway.entries.values() if len(set(e.name.split()).intersection(ko))])
-knownKOSet = set([e for e in pathway.orthologs if len(set(e.name.split()).intersection(ko))])
-
 def colorMapItems(geneSet, color, width):
     for e in geneSet:
 	for g in e.graphics:
@@ -40,13 +27,21 @@ def colorMapItems(geneSet, color, width):
 		g.width = width
 	    g.bgcolor = color
 
-#main
-enhanceSet = knownKOSet 
-#notDE = set([e for e in pathway.entries.values() if not len(set(e.name.split()).intersection(enhanceSet)) and e.type != 'map'])
+
+if not os.path.exists(args.outDir):
+    os.makedirs(args.outDir)
+
+pathway = retrieve_KEGG_pathway(args.path) #pathway of interest
+
+#get KO numbers from previously created file
+ko = ['ko:'+ ko for (trans,sp,ko) in (line.strip().split("\t") for line in (open(args.KO, 'r')))]
+
+knownKOSet = set([e for e in pathway.orthologs if len(set(e.name.split()).intersection(ko))])
+enhanceSet = knownKOSet # legacy from up/down reg script
 notDE = set([e for e in pathway.orthologs if not len(set(e.name.split()).intersection(enhanceSet))])
 
 kgml_map = KGMLCanvas(pathway, show_maps=True)
-kgml_map.import_imagemap = True  
+kgml_map.import_imagemap = True
 #kgml_map.show_maps = False
 kgml_map.show_orthologs = True
 kgml_map.draw_relations = False
@@ -58,9 +53,6 @@ os.chdir(args.outDir)
 koInMap = open(args.path + '_KO.txt', 'w')
 
 colorMapItems(knownKOSet,'#666666', 10)
-for k in knownKOSet:
-    koInMap.write(k.name + '\t' + 'present' + '\n')
-koInMap.close()
 
 # And rendering elements as an overlay
 #kgml_map.show_compounds = True
